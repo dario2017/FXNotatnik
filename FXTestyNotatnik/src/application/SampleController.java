340439bbb222ace208666b43079130b6494c55ae
dd <dd@192.168.8.2>
2017-12-14 17:54:41 +0100

    Initial Commit

---
+++ FXTestyNotatnik/src/application/SampleController.java
@@ -0,0 +1,121 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+public class SampleController {
+	@FXML
+	private Label label1;
+	
+	@FXML
+	private Label label2;
+	
+	@FXML
+	private TextField tytulPole;
+	
+	@FXML
+	private TextArea trescPole;
+	
+	@FXML
+	private Button zapiszBtn;
+	
+	@FXML
+	private Button wczytajBtn;
+	
+	@FXML
+	private Button resetujBtn;
+	
+	
+	public void zapisz(ActionEvent event) {
+		System.out.println("dziala");
+	}
+	
+	
+	public void initialize() {
+		System.out.println("dziala");
+		
+		zapiszBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				System.out.println("zapisuje");
+				try {
+					String czasZapisu = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss").format(new Date());
+					if (tytulPole.getText().isEmpty()) {	
+						tytulPole.setText(czasZapisu);
+					}
+					String nazwaPliku = tytulPole.getText() + ".txt";
+					String sciezkaPliku = "C:/Users/dd/Desktop/Java pliki/Notatnik/" + nazwaPliku;
+					System.out.println(sciezkaPliku);
+					BufferedWriter bw = new BufferedWriter(new FileWriter(sciezkaPliku));
+					bw.write(trescPole.getText());
+					bw.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		wczytajBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				System.out.println("wczytywanie?");
+				FileChooser fc = new FileChooser();
+//				File miejscePliku = new File("C:/Users/dd/Desktop/Java pliki/Notatnik/cos");
+//				fc.setInitialDirectory(miejscePliku);
+				fc.getExtensionFilters().add(new ExtensionFilter("Plik tekstowy", "*.txt"));
+				fc.setTitle("Wybierz plik");
+				File selectedFile = fc.showOpenDialog(new Stage());
+				
+				try {
+					BufferedReader br = new BufferedReader(new FileReader(selectedFile));
+					String wierszZPliku = null;
+					String tekstZPliku = null;
+					while ((wierszZPliku = br.readLine()) != null) {
+						if (tekstZPliku == null) {
+							tekstZPliku = wierszZPliku;
+						} else {
+							tekstZPliku = tekstZPliku + "\n" + wierszZPliku;
+						}
+					}
+					String nazwaPliku = selectedFile.getName();
+					System.out.println(nazwaPliku);
+					String czasAktualny = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss").format(new Date());
+					tytulPole.setText(nazwaPliku + " - wczytano: " + czasAktualny + ".txt");
+					trescPole.setText(tekstZPliku);
+					br.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		
+		resetujBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				String czasAktualny = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss").format(new Date());
+				trescPole.setText("");
+				tytulPole.setText(czasAktualny);
+			}
+		});
+	}
+	
+}
